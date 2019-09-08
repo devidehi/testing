3a6b1cf7760d60ed09f1ff3030d2bfa4ffb14f66
Devi Videhi <devi.videhi@gmail.com>
2019-09-08 10:24:16 +0530

    my comming ater changing author

---
+++ mypythonscript/factorial.py
@@ -0,0 +1,3 @@
+inp=int(input("Enter the number for factorial: ""))
+out=1
+while i<1:
---
+++ mypythonscript/mypythonscripts.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3
+####This one is for finding number between range
+l=[]
+for i in range(2000, 3201):
+    if (i%7==0) and (i%5!=0):
+        l.append(str(i))
+
+print(','.join(l))
